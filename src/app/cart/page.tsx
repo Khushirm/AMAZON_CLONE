10965c3bb80c336ef5daef198f7b8909da00115b
Khushi <khushicshsr2003@gmail.com>
2024-09-22 14:06:12 +0530

    cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,44 +1,48 @@
-"use client";
-import React from 'react'
-import { useSelector } from 'react-redux';
-import CartProduct from '@/components/CartProduct';
-import { StateProps, StoreProduct } from '../../../types';
-import ResetCart from '@/components/ResetCart';
-import Link from 'next/link';
-import CartPayment from '@/components/CartPayment';
-
+"useClient"
+import React from "react";
+import { useSelector } from "react-redux";
+import CartProduct from "@/components/CartProduct";
+import ResetCart from "@/components/ResetCart";
+import Link from "next/link";
+import CartPayment from "@/components/CartPayment";
+import { StateProps, StoreProduct } from "../../../types";
 
 const CartPage = () => {
-    const {productData}=useSelector((state:StateProps)=> state.next);
+  const { productData } = useSelector((state: StateProps) => state.next);
   return (
-    <div className='max-w-screen-2xl mx-auto px-6 grid grid-cols-5 gap-10 py-4'>{productData.length > 0 ? (
+    <div className="max-w-screen-2xl mx-auto px-6 grid grid-cols-5 gap-10 py-4">
+      {productData.length > 0 ? (
         <>
-        <div className='bg-white col-span-4 p-4 rounded-lg'>
-            <div className='flex items-center justify-between border-b-[1px] border-b-gray-400 pb-1'>
-                <p className='text-2xl text-amazon_blue font-semibold'>Shopping Cart</p>
-                <p className='text-lg text-amazon_blue font-semibold'>Subtitle</p>
+          <div className="bg-white col-span-4 p-4 rounded-lg">
+            <div className="flex items-center justify-between border-b-[1px] border-b-gray-400 pb-1">
+              <p className="text-2xl font-semibold text-amazon_blue">
+                Shopping Cart
+              </p>
+              <p className="text-lg font-semibold text-amazon_blue">Subtitle</p>
             </div>
-            <div className='pt-2 flex flex-col gap-2'>
-                {productData.map((item:StoreProduct)=>(
-                    <div key={item._id} >
-                       <CartProduct item={item}/>
-                    </div>
-                ))}
-                <ResetCart/>
+            <div className="pt-2 flex flex-col gap-2">
+              {productData.map((item: StoreProduct) => (
+                <div key={item._id}>
+                  <CartProduct item={item}/>
+                </div>
+              ))}
+              <ResetCart />
             </div>
-        </div>
-        <div className='h-64 col-span-1 p-4 rounded-lg bg-white flex items-center justify-center'>
-            <CartPayment/>
-        </div>
+          </div>
+          <div className="bg-white h-64 col-span-1 p-4 rounded-lg flex items-center justify-center">
+            <CartPayment />
+          </div>
         </>
-    ):( 
-        <div className='bg-white h-64 col-span-5 flex flex-col items-center justify-center py-5 rounded-lg shadow-lg'>
-            <h1 className='text-lg font-medium'>Your cart is empty!</h1>
-            <Link href={"/"}>
-            <button className='w-52 h-10 bg-amazon_blue text-white rounded-lg text-sm font-semibold hover:bg_amazon_yellow hover:text-black'>Do some Shopping Yaaar!!!</button>
-            </Link>
+      ) : (
+        <div className="bg-white h-64 col-span-5 flex flex-col items-center justify-center py-5 rounded-lg shadow-lg">
+          <h1 className="text-lg font-medium">Your cart is empty!</h1>
+          <Link href={"/"}>
+            <button className="w-52 h-10 bg-amazon_blue text-white rounded-lg text-sm font-semibold hover:bg-amazon_yellow hover:text-black">
+              Do some shopping yaar!!
+            </button>
+          </Link>
         </div>
-    )}
+      )}
     </div>
   );
 };
